df4b9e740d237396d9a8f3e3a99d6b5c3163669e
IbrahimAltaf <muhammadibrahimaltaf312@gmail.com>
2026-08-15 13:47:55 +0500

    Fix Vercel build: add --break-system-packages for uv/pip

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py migrate --noinput
 python3 manage.py seed_templates
 python3 manage.py collectstatic --noinput --clear
