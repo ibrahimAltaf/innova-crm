ed1a1fad6333d98c87eeee2406fd1f63756c521a
IbrahimAltaf <muhammadibrahimaltaf312@gmail.com>
2026-08-15 13:59:01 +0500

    Fix static files serving for CSS and assets on Vercel

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("campaigns.urls")),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
+if settings.MEDIA_ROOT:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
